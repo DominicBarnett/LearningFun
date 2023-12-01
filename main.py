de69b3cc666b7c5ff6bd2de752a97c66d6158d2a
DominicBarnett <dominic.barnett@icloud.com>
2023-12-01 10:47:38 -0800

    Added Flask for pages and styles

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """A homepage with handy links for your convenience."""
+    return render_template('main.html')
+
+@app.route('/createaccount')
+def createpage():
+    '''Create an account'''
+    return render_template('create_account.html')
+
+@app.route('/accountcreated')
+def account_created_redirect():
+    '''redirct with a thank you message to child personality'''
+    # thank you for creating account pop up message
+    return render_template('child_personality.html')
+
+@app.route('/childpersonality')
+def childbackground():
+    '''Child background form'''
+    return render_template('child_personality.html')
+
+
+
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(debug=True)
